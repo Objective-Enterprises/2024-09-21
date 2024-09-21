ffd7b9be6337f52f0d113905771651ef4a0f4811
David Y. Stephenson <david@davidystephenson.com>
2024-09-21 12:00:25 -0400

    Fully controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Quiz from "./Quiz";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState('dorothy')
+  function handleChange (event) {
+    setName(event.target.value)
+  }
   return (
     <>
-      <h1>Quiz App</h1>
+      <h1>Quiz App ({name})</h1>
+      <p>What is your name?</p>
+      <input value={name} onChange={handleChange} />
       <Quiz />
     </>
   )
--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,22 @@
+import { useState } from 'react'
+
 export default function Question () {
+  const [answer, setAnswer] = useState('')
+  const first = Math.ceil(Math.random() * 10)
+  const second = Math.ceil(Math.random() * 10)
+  function handleChange (event) {
+    setAnswer(event.target.value)
+  }
+  function handleSubmit (event) {
+    event.preventDefault()
+    const correct = false
+    console.log('correct', correct)
+  }
   return (
-    <input />
+    <form onSubmit={handleSubmit}>
+      <p>{first} + {second} =</p>
+      <input value={answer} onChange={handleChange} />
+      <button>Submit</button>
+    </form>
   )
 }
